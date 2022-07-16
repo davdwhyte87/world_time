d8bc6859f252de8d921456eb683d0a601adc245f
kingstonwhyte87 <kingstonwhyte87@gmail.com>
2022-07-16 18:25:52 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/loading.dart';
+import "pages/home.dart";
+import "pages/choose_location.dart";
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/':(context)=>Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
+
