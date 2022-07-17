429d42923e61d2c0a1e790651a8434bc70690287
kingstonwhyte87 <kingstonwhyte87@gmail.com>
2022-07-17 19:04:47 +0100

    add loading animation

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
@@ -31,7 +32,11 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Loading ........")
+      backgroundColor: Colors.blue[900],
+      body:  const SpinKitCircle(
+        color: Colors.white,
+        size: 80.0,
+      )
     );
   }
 }
